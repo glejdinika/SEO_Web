8deeccb63c21c234aceeb1806c0cb8f2e126c771
Glejdi <glenik@yea.al>
2026-03-04 12:28:58 +0100

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import pic1 from '$lib/assets/pic1.jpg';
+</script> 
+
+<header class="h-125 bg-center bg-cover flex items-center justify-center" 
+style="background-image: url({pic1})">
+     <h1 class="text-5xl">Electronic Device</h1>
+</header>
+
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
