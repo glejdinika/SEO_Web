1e88ae1d251ac2a6429dc87922958e32ac68e159
Glejdi <glenik@yea.al>
2026-03-04 12:40:21 +0100

    second

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
     import pic1 from '$lib/assets/pic1.jpg';
 </script> 
 
+
+<svelte:head>
+    <title>Electronic for all - Niko here</title>
+    <meta name="description" content="Electronic device for all. Niko here" />
+</svelte:head>
+
 <header class="h-125 bg-center bg-cover flex items-center justify-center" 
 style="background-image: url({pic1})">
      <h1 class="text-5xl">Electronic Device</h1>
